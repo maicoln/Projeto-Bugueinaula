014213f2aa04bb158a7ebf76c13f952088899a1c
maicoln <maicolnsilva@gmail.com>
2025-09-11 11:40:12 -0300

    Fix: Deploy Bugueinaula 3.0

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { supabase } from '@/lib/supabaseClient';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
